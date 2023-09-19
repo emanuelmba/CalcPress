f80dcce813c53aafc7b08ef5782eb710ddeaaf19
emanuelmba <emanuelbailo@gmail.com>
2023-09-19 10:53:24 -0300

    semantic changes

--- calcpress.js
+++ calcpress.js
@@ -6,65 +6,54 @@ fetch('https://api.bluelytics.com.ar/v2/latest')
   .then((data) => {
     blue = data.blue.value_buy
     blue = (blue - blue * 0.0066).toFixed(2)
-    document.getElementById('coti1').value = blue
-    document.getElementById('coti2').value = blue
+    document.getElementById('cotiPayPal').value = blue
+    document.getElementById('cotiMpago').value = blue
   })
 
 //función para calcular PayPal
 function paypal() {
-  let text1 = ''
-  let usd = Number(document.getElementById('usd').value)
-  let coti = Number(document.getElementById('coti1').value)
-  let ppal = usd - (usd * 5.4) / 100 - 0.3
-  let airtm = ppal / ppToAtm
+  let text
+  let usd = +document.getElementById('usd').value
+  let coti = +document.getElementById('cotiPayPal').value
+  let inter = usd - (usd * 5.4) / 100 - 0.3
+  let airtm = inter / ppToAtm
   let mpago = airtm * coti
   if (isNaN(usd)) {
-    text1 = 'Por favor, ingrese un número correcto.'
+    text = 'Por favor, ingrese un número correcto.'
   } else {
-    text1 =
-      'Con $' +
-      usd +
-      ' recibes $' +
-      ppal.toFixed(2) +
-      '.<br>Airtm: $' +
-      airtm.toFixed(2) +
-      '.<br>MercadoPago: $' +
-      mpago.toFixed(0) +
-      '.'
+    text = `Con $${usd} recibes $${inter.toFixed(2)}.<br>
+      Airtm: $${airtm.toFixed(2)}.<br>
+      MercadoPago: $${Math.round(mpago)}.`
   }
-  document.getElementById('resu1').innerHTML = text1
+  document.getElementById('resultPayPal').innerHTML = text
 }
-document.getElementById('reset1').addEventListener('click', function () {
+document.getElementById('resetPayPal').addEventListener('click', function () {
+  document.getElementById('cotiPayPal').value = blue
+  document.getElementById('resultPayPal').innerHTML = ''
   document.getElementById('usd').value = ''
-  document.getElementById('coti1').value = blue
-  document.getElementById('resu1').innerHTML = ''
 })
 
 //función para calcular MercadoPago
 function mpago() {
-  let text2 = ''
-  let ars = Number(document.getElementById('ars').value)
-  let coti = Number(document.getElementById('coti2').value)
-  let airtm = (ars / coti) * ppToAtm
-  let ppal = airtm + (airtm * 5.4) / 100 + 0.3
+  let text
+  let ars = +document.getElementById('ars').value
+  let coti = +document.getElementById('cotiMpago').value
+  let airtm = ars / coti
+  let inter = airtm * ppToAtm
+  let ppal = inter + (inter * 5.4) / 100 + 0.3
   if (isNaN(ars)) {
-    text2 = 'Por favor, ingrese un número correcto.'
+    text = 'Por favor, ingrese un número correcto.'
   } else {
-    text2 =
-      'Para recibir $' +
-      ars +
-      '<br>necesitas $' +
-      ppal.toFixed(2) +
-      ' en PayPal.<br>Airtm: $' +
-      (ars / coti).toFixed(2) +
-      '.'
+    text = `Para recibir $${ars}<br>
+      necesitas $${Math.ceil(ppal)} en PayPal.<br>
+      Airtm: $${airtm.toFixed(2)}.`
   }
-  document.getElementById('resu2').innerHTML = text2
+  document.getElementById('resultMpago').innerHTML = text
 }
-document.getElementById('reset2').addEventListener('click', function () {
+document.getElementById('resetMpago').addEventListener('click', function () {
+  document.getElementById('cotiMpago').value = blue
+  document.getElementById('resultMpago').innerHTML = ''
   document.getElementById('ars').value = ''
-  document.getElementById('coti2').value = blue
-  document.getElementById('resu2').innerHTML = ''
 })
 
 //funciones con Enter
